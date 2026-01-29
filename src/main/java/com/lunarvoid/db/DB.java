70fe8106d1616d1f73b59604c2c82841c2b60812
Pedro Maia <pedrodrm250@gmail.com>
2026-01-29 19:07:59 -0300

    pronto para iniciar implementação

--- src/main/java/com/lunarvoid/db/DB.java
+++ src/main/java/com/lunarvoid/db/DB.java
@@ -1,18 +1,30 @@
 package com.lunarvoid.db;
 
-    import java.sql.Connection;
-    import com.lunarvoid.utilitarios.Config;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
-public class DB {
+import com.lunarvoid.utilitarios.Config;
+
+public final class DB {
     private static final String URL = Config.get("db.url");
     private static final String USER = Config.get("db.user");
     private static final String PASSWORD = Config.get("db.password");
     private static Connection conn = null;
 
-    static{
-
-    }
-
     public static Connection getConnection(){
+        try{
+
+            if(conn == null || conn.isClosed()){
+                conn = DriverManager.getConnection(URL, USER, PASSWORD);
+            }
+
+            return conn;
+            
+        }catch(SQLException s){
+            throw new RuntimeException("Falha ao acessar banco de dados" + s.getMessage());
+        }
     }
+
+    private DB(){}
 }
